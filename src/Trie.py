2392c6b19d8423ba4e2009b613298cb79e28fe5d
rorychatt <pavel.rinne@gmail.com>
2024-05-27 10:55:32 +0200

    trie class

---
+++ src/Trie.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+from trie_node import TrieNode
+
+@dataclass
+class Trie:
+    root: TrieNode = field(default_factory=TrieNode)
+
+    def insert(self, prefix: str, price: float) -> None:
+        node = self.root
+        for digit in prefix:
+            if digit not in node.children:
+                node.children[digit] = TrieNode()
+            node = node.children[digit]
+        node.price = price
+
+    def search(self, phone_number: str) -> float:
+        node = self.root
+        current_price = float('inf')
+        for digit in phone_number:
+            if digit in node.children:
+                node = node.children[digit]
+                if node.price < current_price:
+                    current_price = node.price
+            else:
+                break
+        return current_price
